c8a982ed460cce06b9710a69f6bd7335f2bc9f2c
zelihauslu <zelusluz@gmail.com>
2022-07-30 22:19:05 +0300

    project created

---
+++ Map-App/Map-App/Map_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_AppApp.swift
+//  Map-App
+//
+//  Created by Zeliha Uslu on 30.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Map_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
