0b9713f006ee517548a9027c1b2808c8b1e3a6ba
zelihauslu <zelusluz@gmail.com>
2022-07-30 22:44:47 +0300

    LocationModel and DataService created

---
+++ Map-App/Map-App/Models/Location.swift
@@ -0,0 +1,33 @@
+//
+//  Location.swift
+//  Map-App
+//
+//  Created by Zeliha Uslu on 30.07.2022.
+//
+
+import Foundation
+import MapKit
+
+struct Location{
+    let name : String
+    let cityName : String
+    let coordinates : CLLocationCoordinate2D
+    let description : String
+    let imageNames : [String]
+    let link : String
+}
+
+
+/*
+ Location(
+     name: "Colosseum",
+     cityName: "Rome",
+     coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
+     description: "The Colosseum is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum. It is the largest ancient amphitheatre ever built, and is still the largest standing amphitheatre in the world today, despite its age.",
+     imageNames: [
+         "rome-colosseum-1",
+         "rome-colosseum-2",
+         "rome-colosseum-3",
+     ],
+     link: "https://en.wikipedia.org/wiki/Colosseum")
+ */
