03086816bb3436f0822c85b10a3a2bca05870caf
zelihauslu <zelusluz@gmail.com>
2022-07-30 23:45:56 +0300

    current location operations done

--- Map-App/Map-App/ViewModels/LocationsViewModel.swift
+++ Map-App/Map-App/ViewModels/LocationsViewModel.swift
@@ -6,14 +6,38 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel : ObservableObject{
     
     @Published var locations : [Location]
     
+    //current location on map
+    @Published var mapLocation : Location{
+        didSet{
+            updateMapRegion(location: mapLocation)
+        }
+    }
+
+    @Published var mapRegion : MKCoordinateRegion = MKCoordinateRegion()
+    
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init(){
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+        
     }
     
+    private func updateMapRegion(location : Location){
+        withAnimation(.easeInOut){
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
+    }
 }
