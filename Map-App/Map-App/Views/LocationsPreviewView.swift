e7aec15a6c6c7c9b44f427e4a295fc3de8fb2141
zelihauslu <zelusluz@gmail.com>
2022-07-31 09:27:42 +0300

    LocationPreviewView created

---
+++ Map-App/Map-App/Views/LocationsPreviewView.swift
@@ -0,0 +1,94 @@
+//
+//  LocationsPreviewView.swift
+//  Map-App
+//
+//  Created by Zeliha Uslu on 31.07.2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationsPreviewView: View {
+    
+    let location : Location
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            VStack(alignment: .leading, spacing: 16){
+                ImageSection
+                TextSection
+            }
+            
+            VStack(spacing: 8){
+                learnMoreButton
+                nextButton
+            }
+        } .padding(20)
+            .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.ultraThinMaterial)
+                .offset(y: 65)
+            )
+            .padding()
+            .cornerRadius(10)
+    }
+}
+
+struct LocationsPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            LocationsPreviewView(location : LocationsDataService.locations.first!)
+        }
+    }
+}
+
+extension LocationsPreviewView{
+    
+    private var ImageSection: some View{
+        ZStack {
+            if let imageNames = location.imageNames.first{
+                Image(imageNames)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }.padding(6)
+            .background(.white)
+            .cornerRadius(10)
+        
+    }
+    
+    private var TextSection : some View{
+        VStack{
+            Text(location.name)
+                .font(.title2)
+                .bold()
+            Text(location.cityName)
+                .font(.subheadline)
+        }.frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var learnMoreButton : some View{
+        Button {
+        //
+        } label: {
+            Text("Learn More")
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton : some View{
+        Button {
+        //
+        } label: {
+            Text("Next")
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.bordered)
+    }
+}
+
+
